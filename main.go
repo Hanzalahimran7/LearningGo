dcfd08710916cc5dc60212b4074ad9eff1a34e4b
hanzalahimran7 <hanzalahimran9@gmail.com>
2024-05-16 06:39:06 +0000

    Added dotenv package

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port not found in env file")
+	}
+	fmt.Println(port)
 }
