2d58f7c8a06165f1c9ec09d67276943db9c0f5a7
hanzalahimran7 <hanzalahimran9@gmail.com>
2024-05-16 11:00:11 +0000

    Added response writers and routing

--- main.go
+++ main.go
@@ -32,6 +32,11 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handleReadiness)
+	v1Router.Get("/error", handleError)
+
+	router.Mount("/v1", v1Router)
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + port,
